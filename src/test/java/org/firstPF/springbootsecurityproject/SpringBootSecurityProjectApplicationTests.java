5176513a98f1753873587ac6e379aaf41a26ae7e
Matyáš Bureš <117597975+MatBures@users.noreply.github.com>
2023-08-02 12:15:54 +0200

    Initial commit

---
+++ src/test/java/org/firstPF/springbootsecurityproject/SpringBootSecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.firstPF.springbootsecurityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
