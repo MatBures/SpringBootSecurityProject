063cf7915f8dc4b5b6b153b0f179e3236913639a
Matyáš Bureš <117597975+MatBures@users.noreply.github.com>
2023-08-08 11:08:10 +0200

    Added AuthenticationService

--- src/main/java/org/firstPF/auth/AuthenticationController.java
+++ src/main/java/org/firstPF/auth/AuthenticationController.java
@@ -12,17 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
-    @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(
-            @RequestBody RegisterRequest request
-    ) {
 
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+
+        return ResponseEntity.ok(authenticationService.register(request));
     }
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate(
-            @RequestBody AuthenticationRequest request
-    ) {
-
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
+        return ResponseEntity.ok(authenticationService.authenticate(request));
     }
 
 
